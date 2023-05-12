c9e87140087031645c00c5540648b817361af5a3
Terlale <trllramazanova@gmail.com>
2023-05-13 02:17:04 +0400

    start

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Premium from './components/Premium';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Premium />
   </React.StrictMode>
 );
 
